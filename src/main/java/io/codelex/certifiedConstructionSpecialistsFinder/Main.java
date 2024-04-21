47d7ced728f40f4e50015fdcad9cf8d3f0d34c36
Kalvis <kalvis.kaulins@inbox.lv>
2024-04-22 00:33:12 +0300

    Revert "Finished construction-specialists-finder"
    This reverts commit de1189dfd98a03eb493fd9dc61c367c9f114da64.

--- src/main/java/io/codelex/certifiedConstructionSpecialistsFinder/Main.java
+++
@@ -1,8 +0,0 @@
-package io.codelex.certifiedConstructionSpecialistsFinder;
-
-public class Main {
-    public static void main(String[] args) {
-        FindData findData = new FindData();
-        findData.getUserSelectedInfoBySearchCriteria();
-    }
-}
